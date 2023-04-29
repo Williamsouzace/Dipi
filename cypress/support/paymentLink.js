3eb12947d1204e1ac3628019bf4e8b3c9c44482e
WilliamQa30 <Williadesousa30@gmail.com>
2023-04-28 22:13:29 -0300

    subir

--- cypress/support/paymentLink.js
+++ cypress/support/paymentLink.js
@@ -1,4 +1,5 @@
 Cypress.Commands.add('checkRequiredFields', () => {
     cy.visit('/payments-link')
     cy.get('[class="btn btn-primary"').click()
+    cy.get('#StoreId').select('XIAOMI BRASIL COMERCIO DE ELETRONICOS EIRELI teste')
 })

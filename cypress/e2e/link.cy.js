b0ffe6eef30f0d78d604c38dcfede77cc85b94fa
WilliamQa30 <Williadesousa30@gmail.com>
2023-05-08 23:33:57 -0300

    agora 08/05

--- cypress/e2e/link.cy.js
+++ cypress/e2e/link.cy.js
@@ -8,13 +8,17 @@ describe('Link de Pagamento', () => {
     it('Criando link e cancelando', () => {
         let portion = 'Em até 1x'
         let date = '10102030'
-        cy.paymentCreate(portion,date)
-        cy.paymentCancel()    
+        cy.paymentCreate(portion, date)
+        cy.paymentCancel()
+    })
+    it('Gerar um link de pagamento data invalida', () => {
+        let portion = 'Em até 1x'
+        cy.paymentCreate_date_invalid(portion)
     })
     it('Gerar um link de pagamento válido', () => {
         let portion = 'Em até 1x'
         let date = '10102030'
-        cy.paymentCreate(portion,date)
+        cy.paymentCreate(portion, date)
     })
     it('Realizar pagamento e verificação de status', () => {
         let status = 'Paga'
@@ -23,28 +27,29 @@ describe('Link de Pagamento', () => {
         cy.creditPayment(portion)
         cy.verification_Status(status)
     })
-    it.only('Gerar um link de pagamento parcelado', () => {
+    it('Gerar um link de pagamento parcelado', () => {
         let portion = 'Em até 5x'
         let date = '101030'
-        cy.paymentCreate(portion,date)
+        cy.paymentCreate(portion, date)
     })
-    it.only('Realizar pagamento e verificação de status', () => {
+    it('Realizar pagamento e verificação de status', () => {
         let status = 'Paga'
         let portion = '5'
         cy.linkPayment()
         cy.creditPayment(portion)
         cy.verification_Status(status)
     })
-    it('Gerar um link de pagamento', () => {
-        let portion = 'Em até 1x'
-        let date = '10102030'
-        cy.paymentCreate(portion,date)
-    })
     it('Realizar pagamento com debito e verificação de status', () => {
         let status = 'Paga'
+        let portion = 'Em até 1x'
+        let date = '10102030'
+        cy.paymentCreate(portion, date)
         cy.linkPayment()
         cy.debtPayment()
         cy.verification_Status(status)
+    })
+    it('Verificação dos filtros', () => {
+        cy.paymentLinkFilters()
     })
 
 })
--- cypress/support/paymentLink.js
+++ cypress/support/paymentLink.js
@@ -1,31 +1,32 @@
 import { faker } from '@faker-js/faker'
 let cnpj = faker.datatype.number({ min: 10000000000000 })
 let valor = '0.12'
-let loja = 'XIAOMI BRASIL COMERCIO DE ELETRONICOS EIRELI teste'
+let loja = 'EMPRESA PARA TESTE'
+let cliente = 'Teste William'
 const cardName = Cypress.env('nome_Cartao')
 const numeroCartao = Cypress.env('numeroCartao')
 const cod_Seg = Cypress.env('cod_Seg')
 const data_Exp = Cypress.env('data_Exp')
-//Campos obrigatórios
+//-------------------------------------------------------------------------Campos obrigatórios---------------------------------------------------------------//
 Cypress.Commands.add('checkRequiredFields', () => {
     cy.visit('/payments-link')
     cy.get('[class="btn btn-primary"').click()
     cy.contains('Confirmar').click().should('be.visible')
     cy.contains('Cancelar').click().should('be.visible')
 })
-//Gerar link de pagamento 
-Cypress.Commands.add('paymentCreate', (portion,date) => {
+//-----------------------------------------------------------------------Gerar link de pagamento---------------------------------------------------------------//
+Cypress.Commands.add('paymentCreate', (portion, date) => {
     cy.visit('/payments-link')
     cy.get('[class="btn btn-primary"').click()
     cy.get('#StoreId').select(loja)
-    cy.get('[name="CustomerName"').type('Teste William')
+    cy.get('[name="CustomerName"').type(cliente)
     cy.get('[name="CustomerIdentity"').type(cnpj)
     cy.get('[name="ZipCode"').type('62322365')
     cy.get('[name="Number"').type('100')
     cy.get('[class="form-control"').eq(11).type(valor)
     cy.get('[class="form-control calendar-border"').clear().type(date)
     cy.get('#Boleto').click({ force: true })
-    cy.get('#Installments').select(portion,{force:true})
+    cy.get('#Installments').select(portion, { force: true })
     cy.wait(2000)
     cy.intercept('POST', '/api/link/create').as('request')
     cy.contains('Confirmar').click().should('be.visible')
@@ -35,6 +36,22 @@ Cypress.Commands.add('paymentCreate', (portion,date) => {
             cy.task('saveNoteId', response.body.Id)
         })
     cy.get('@request').its('response.statusCode').should('eq', 201)
+})
+Cypress.Commands.add('paymentCreate_date_invalid', (portion) => {
+    cy.visit('/payments-link')
+    cy.get('[class="btn btn-primary"').click()
+    cy.get('#StoreId').select(loja)
+    cy.get('[name="CustomerName"').type('Teste William')
+    cy.get('[name="CustomerIdentity"').type(cnpj)
+    cy.get('[name="ZipCode"').type('62322365')
+    cy.get('[name="Number"').type('100')
+    cy.get('[class="form-control"').eq(11).type(valor)
+    cy.get('[class="form-control calendar-border"').clear().type('07052023')
+    cy.get('#Boleto').click({ force: true })
+    cy.get('#Installments').select(portion, { force: true })
+    cy.wait(2000)
+    cy.contains('Confirmar').click().should('be.visible')
+    cy.get('[class="Toastify__toast-body"').should('have.text', 'Erro ao gerar link de pagamento!')
 })
 //Acessar ultimo link de pagamento criado
 Cypress.Commands.add('linkPayment', () => {
@@ -50,7 +67,7 @@ Cypress.Commands.add('linkPayment', () => {
 Cypress.Commands.add('paymentCancel', () => {
     cy.visit('/payments-link')
     cy.get('[class="btn btn-outline-secondary"').click()
-    cy.get('[name="search_term"').type('Teste William')
+    cy.get('[name="search_term"').type(cliente)
     cy.get('[class="waves-effect btn btn-primary"').click()
     cy.get('[class="MuiButtonBase-root MuiIconButton-root MuiIconButton-sizeMedium css-1ua49gz"').first().click({ force: true })
     cy.contains('Cancelar link de pagamento').click()
@@ -103,4 +120,51 @@ Cypress.Commands.add('verification_Status', (status) => {
 
         checkStatus();
     });
+})
+Cypress.Commands.add('paymentLinkFilters', () => {
+    cy.visit('/payments-link')
+    cy.viewport(1920, 1080);
+    cy.get('[class="btn btn-outline-secondary"').click()
+    cy.get('[name="search_term"').type(cliente)
+    cy.get('[class="select__value-container select__value-container--is-multi css-1hwfws3"').first().click().type('EMPRESA PARA TESTE{enter}')
+    cy.get('[name="CustomerName"').type(cliente)
+    //cy.get('[name="CustomerIdentity"').type(cnpj)
+    cy.get('[placeholder="Data de criação"').type('10/10/2030')
+    //cy.get('[placeholder="Data de vencimento"').type('08/05/2023')
+    //---------------------------------------------------------------------elementos de situação---------------------------------------------------------------------------//
+    cy.get('[class="select__value-container select__value-container--is-multi css-1hwfws3"').eq(0).click().type('Pendente{enter}')
+    cy.get('[type="submit"').click({ force: true })
+    cy.get('[class="btn btn-outline-secondary"').click()
+    cy.contains('Pendente').should('be.visible')
+    cy.get('[class="btn btn-outline-secondary"').click()
+    cy.get('[class="css-8mmkcg"').eq(2).click()
+    cy.get('[class="select__value-container select__value-container--is-multi css-1hwfws3"').eq(0).click().type('Pago{enter}')
+    cy.get('[type="submit"').click({ force: true })//Botão de filtrar
+    cy.get('[class="badge badge-light-success badge-pill"').first().should('have.text', 'Paga')
+    cy.get('[class="css-8mmkcg"').eq(2).click()
+    cy.get('[class="select__value-container select__value-container--is-multi css-1hwfws3"').eq(0).click().type('Cancelado{enter}')
+    cy.get('[type="submit"').click({ force: true })//Botão de filtrar
+    cy.get('[class="btn btn-outline-secondary"').click()
+    cy.contains('Cancelado').should('be.visible')
+    cy.get('[class="btn btn-outline-secondary"').click()
+    cy.get('[class="css-8mmkcg"').eq(2).click()
+     //---------------------------------------------------------------------elementos de pago com:---------------------------------------------------------------------------//
+    cy.get('[class="select__value-container select__value-container--is-multi css-1hwfws3"').last().click().type('Aguardando{enter}')
+    cy.get('[type="submit"').click({ force: true })//Botão de filtrar
+    cy.get('[class="btn btn-outline-secondary"').click()
+    cy.contains('Aguardando pagamento').should('be.visible')
+    cy.get('[class="btn btn-outline-secondary"').click()
+    cy.get('[class="css-8mmkcg"').eq(3).click()
+    cy.get('[class="select__value-container select__value-container--is-multi css-1hwfws3"').last().click().type('Crédito{enter}')
+    cy.get('[type="submit"').click({ force: true })//Botão de filtrar
+    cy.get('[class="btn btn-outline-secondary"').click()
+    cy.contains('Cartão de Crédito').should('be.visible')
+    cy.get('[class="btn btn-outline-secondary"').click()
+    cy.get('[class="css-8mmkcg"').eq(3).click()
+    cy.get('[class="select__value-container select__value-container--is-multi css-1hwfws3"').last().click().type('Pix{enter}')
+    cy.get('[type="submit"').click({ force: true })//Botão de filtrar
+    cy.get('[class="btn btn-outline-secondary"').click()
+    cy.contains('Pix').should('be.visible')
+    cy.get('[class="btn btn-outline-secondary"').click()
+    cy.get('[class="css-8mmkcg"').eq(3).click()
 })
